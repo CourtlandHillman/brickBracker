f4b54d535f4e0b409e0ee1126153524e753cfde2
Maksym Vdovenko <jackjackjack1376@gmail.com>
2024-04-01 20:10:33 +0900

    Added gameplay for bricks and ball.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame obj = new JFrame();
+        GamePlay gamePlay = new GamePlay();
+        obj.setVisible(true);
+        obj.setBounds(10 , 10 , 700 , 600);
+        obj.setTitle("Breakout Ball");
+        obj.setResizable(false);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
     }
 }
